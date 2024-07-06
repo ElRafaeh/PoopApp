0307e9773479c038ff7e17dd3ea7dbb2bba47de0
Rafa <rbm71@alu.ua.es>
2024-07-06 11:55:08 +0200

    Testing application endpoints

--- backend/src/main/java/com/PoopApp/Controllers/UserController.java
+++ backend/src/main/java/com/PoopApp/Controllers/UserController.java
@@ -1,19 +1,35 @@
 package com.PoopApp.Controllers;
 
+import com.PoopApp.Entities.User;
 import com.PoopApp.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("")
-    public String holaMundo() {
+    @GetMapping("/")
+    public String getUsers() {
         return "MARICON";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById() {
+        return "MARICON";
+    }
+
+    @PostMapping("/")
+    public User user(
+            @RequestParam String nickname ,
+            @RequestParam String name,
+            @RequestParam String surname,
+            @RequestParam String lastName,
+            @RequestParam String email,
+            @RequestParam String password
+            ) {
+        return userRepository.save();
     }
 }
--- backend/src/main/java/com/PoopApp/Entities/User.java
+++ backend/src/main/java/com/PoopApp/Entities/User.java
@@ -94,4 +94,17 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
